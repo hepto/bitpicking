04c8b80002df70fb24b3f141f7e5240e225bb74e
Greg Hepworth <greg.hepworth@naimaudio.com>
2019-05-22 11:20:22 +0100

    Moving to FeedrApp as Bitnami doesn't work in production without paying

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,24 +1,14 @@
-$(document).ready(function() {
-	
-	var url = 'https://anchor.fm/s/69da638/podcast/rss';
-	
-	feednami.load(url,function(result){
-		if(result.error) {
-			console.log(result.error);
-		} else {
-			var entries = result.feed.entries;
-			var episodes = $('#episodes');
-			for(var i = 0; i < entries.length; i++){
-				var entry = entries[i];
-				var li = $('<li/>')
-					.addClass('episode')
-			        .appendTo(episodes);
-			    var link = $('<a/>')
-			        .text(entry.title)
-			        .attr('href', entry.link)
-			        .appendTo(li);
-			}
-		}
-	});
-	
-});
+renderRSS = function(rss){
+  	var entries = rss.responseData.feed.entries;
+	var episodes = $('#episodes');
+	for(var i = 0; i < entries.length; i++){
+		var entry = entries[i];
+		var li = $('<li/>')
+			.addClass('episode')
+	        .appendTo(episodes);
+	    var link = $('<a/>')
+	        .text(entry.title)
+	        .attr('href', entry.link)
+	        .appendTo(li);
+	}
+};
