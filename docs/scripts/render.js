fdc814f7c36fa8d9baafb06e9ab4fa2e04e82113
Greg Hepworth <greg.hepworth@naimaudio.com>
2019-05-22 18:34:46 +0100

    Once again, needed to specify HTTPS for the API to work in Safari

--- docs/scripts/render.js
+++ docs/scripts/render.js
@@ -1,5 +1,6 @@
 jQuery(function($) {
 	$("#feed").rss("https://anchor.fm/s/69da638/podcast/rss", {
+		ssl: true,
 		limit: 100,
 		layoutTemplate: "<ul>{entries}</ul>",
 		entryTemplate: "<li><a href='{url}'>{title}</li>"
